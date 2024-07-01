c041536bc9a4b7010a02d9148e81da71710509a6
rajnish <rajnishkumar550443@gmail.com>
2024-07-01 22:50:16 +0530

    Initial commit

---
+++ variable/hello.js
@@ -0,0 +1,18 @@
+// var 
+var a = 10;
+    a=20; //we can re-initialize var
+var a = 30;//we can proform re-declearation  with var
+console.log(a);
+
+// let
+let c =5;
+// let c= 8; //we cant re-declera let
+c =10; //we can re initialize
+console.log(c);
+
+
+// constent
+const d = 15;
+ // d = 20; //we cant re-declare const
+ // d = 30; //we cant re-initialize const
+ console.log(d);
